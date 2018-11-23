5d4beefcb8727acd52875c20354f702b6d21a7f7
Renzo Rios <rios.renzo@pucp.pe>
2018-11-23 14:25:44 -0500

    A simple configuration

--- setIdElement/setIdElement/setIdElement.cs
+++ setIdElement/setIdElement/setIdElement.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//Name Spaces Add
+using Autodesk.Revit.DB;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.Attributes;
+using System.Diagnostics;
+using System.Reflection;
+using Autodesk.Revit.DB.Structure;
 
 namespace setIdElement
 {
-    public class setIdElement
+    [Transaction(TransactionMode.Manual)]
+    public class setIdElement : IExternalCommand
     {
+        public Result Execute(ExternalCommandData commandData, 
+                              ref string message, 
+                              ElementSet elements)
+        {
+            TaskDialog.Show("Message Test", "Hello World");
+
+            return Result.Succeeded;
+        }
     }
 }
